6df195998497dae4ddb8e9b49ae4f85382b64de8
Sado-Shi <derasado04@gmail.com>
2021-11-16 19:02:14 +0900

    オブジェクト指向部分完走

---
+++ selfphp/chap10/aa.php
@@ -0,0 +1,6 @@
+<?php
+ini_set("error_reporting", E_ALL);
+require_once "bb.php";
+
+$bb = new MyClass();
+$bb->hoge;
--- selfphp/chap10/auto_load1.php
+++ selfphp/chap10/auto_load1.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'Autoloader.php';
+ini_set("error_reporting", E_ALL);
 
 $p = new Person('太郎', '山田');
+print_r(ini_get_all());
 $p->show();
